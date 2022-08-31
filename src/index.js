75c8bdc1429d04e9658dd117cad562818eaccf04
Ashton MacKenzie <pikeminnow@gmail.com>
2022-08-31 18:48:41 -0400

    suppress default submit, read task description
    have the task reading into memory however for some reason unsure how to actually append this task description into a <li> inserted into the correct location. am approaching this in a different  way to the lecturer code example because I am not storing this data in a separate file. will check previous labs on modifying elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+debugger;
+  document.getElementById("create-task-form").addEventListener('submit', (event) => {
+    debugger;
+    event.preventDefault();
+    const task = document.getElementById("new-task-description").value;
+    debugger;
+    const li = document.createElement('li');
+    debugger;
+    const taskLi = li.append(task);
+    debugger;
+    document.getElementById("tasks").append(li);
+    debugger;
+  });
 });
+
+// document.querySelector('#book-form').addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   // Create a New Book Object Using Form Values
+
+//       // console.log(e.target["form-title"].value);
+//       console.log(e.target.title.value);
+//       console.log(e.target.author.value);
+//       console.log(e.target.price.value);
+//       console.log(e.target.imageUrl.value);
+//       console.log(e.target.inventory.value);
+
+//   // Fire Off renderBookCard With New Book Object
+// });
